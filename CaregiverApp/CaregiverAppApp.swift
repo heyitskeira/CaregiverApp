d09e0e41946b03c1935ac390f722c505a585a0c3
pp <pp>
2026-05-26 08:40:07 +0800

    Initial Commit

---
+++ CaregiverApp/CaregiverAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaregiverAppApp.swift
+//  CaregiverApp
+//
+//  Created by Keira on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CaregiverAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
