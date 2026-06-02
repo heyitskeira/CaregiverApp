e7a6f9ee7d364bc2a0318e7290a164e044c71261
Dzikry Aji Santoso <100667379+dzikryaji@users.noreply.github.com>
2026-06-02 09:32:14 +0800

    feat: update color palette

--- CaregiverApp/Color.swift
+++ CaregiverApp/Color.swift
@@ -4,10 +4,9 @@
 import SwiftUI
 
 extension Color {
-    static let paletteBlue      = Color(red: 0.086, green: 0.211, blue: 0.4)   // #163666
-    static let paletteGreen     = Color(red: 0.32, green: 0.76, blue: 0.54)   // #52C18A
-    static let paletteLightYellow = Color(red: 0.953, green: 0.961, blue: 0.639) // #F3F5A3
-    static let paletteYellow    = Color(red: 1.0, green: 0.831, blue: 0.282)  // #FFD448
-    static let paletteOrange    = Color(red: 0.957, green: 0.439, blue: 0.224) // #F47039
-    static let palettePurple    = Color(red: 0.71, green: 0.298, blue: 0.565)  // #B54C90
+    static let paletteBlue      = Color("AccentColor")
+    static let paletteOrange    = Color("Orange")
+    static let paletteYellow    = Color("Yellow")
+    static let paletteRed       = Color("Red")
+    static let palettePurple    = Color("Purple")
 }
