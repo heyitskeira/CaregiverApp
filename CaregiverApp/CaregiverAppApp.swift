c288d9907f1f9d458a8859f07dcb8402bdc0a23d
Dzikry Aji Santoso <100667379+dzikryaji@users.noreply.github.com>
2026-06-10 09:46:30 +0800

    feat: add splash screen

--- CaregiverApp/CaregiverAppApp.swift
+++ CaregiverApp/CaregiverAppApp.swift
@@ -19,14 +19,33 @@ enum AppTheme: String {
 
 @main
 struct CaregiverAppApp: App {
+    @State private var showSplash = true
     @State private var router = AppRouter()
-    @AppStorage("theme") private var theme = AppTheme.light.rawValue
+
+    @AppStorage("theme")
+    private var theme = AppTheme.light.rawValue
 
     var body: some Scene {
         WindowGroup {
-            RootView()
-                .environment(router)
-                .preferredColorScheme(preferredColorScheme)
+            ZStack {
+                RootView()
+                    .environment(router)
+
+                if showSplash {
+                    SplashView()
+                        .transition(.opacity)
+                        .zIndex(1)
+                }
+            }
+            .animation(.easeInOut(duration: 0.5), value: showSplash)
+            .preferredColorScheme(preferredColorScheme)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation(.easeInOut(duration: 0.5)) {
+                        showSplash = false
+                    }
+                }
+            }
         }
     }
 
